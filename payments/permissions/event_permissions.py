2ad4708fe0a66d949e7e4a5b27b33422cefd3764
ayahusu <ayahusu2210@gmail.com>
2026-08-27 08:44:07 +0545

    refactor(payments): remove payments app and unused registration service

--- payments/permissions/event_permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsEventOrganizer(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return obj.organizer == request.user
